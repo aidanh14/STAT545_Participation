0f9d02a7ce9a44183952c10fe9ac758f5b5baa14
Aidan Hughes <aidan.hughes14@gmail.com>
2018-11-08 10:37:11 -0800

    committing package-making project

---
+++ STAT547/powers/R/powers-package.R
@@ -0,0 +1,5 @@
+#' @keywords internal
+#' @details Details go here
+"_PACKAGE"
+
+## Note: You need to run the `document()` function for roxygen to convert all the labels
---
+++ STAT547/powers/R/square.R
@@ -0,0 +1,6 @@
+#' @title Square the input
+#' @param x Vector of numerics
+#' @return The vector x, squared.
+#' @export
+## Note: need to run `document()` function from roxygen2 (part of `devtools` package) to generate the documentation files.
+square <-  function(x) pow(x, 2)
